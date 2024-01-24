bfaff85f47a9069ce9c96a19127c8224d4d2b564
chaayee0123 <chayee0614@gmail.com>
2024-01-24 17:16:51 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import { Component } from "react";
 import "./App.css";
-export default class App extends Component {
-  render() {
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import React, { useEffect, useState } from "react";
+
+
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 2, charge: '빵', amount: 1000 },
+    { id: 3, charge: '맥북', amount: 3000 }
+  ]);
+
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  }
+  const handleAmount = (e) => {
+    setAmount(e.target.valueAsNumber);
+  }
+
+  const handleDelete = (id) => {
+    const newExpense = expenses.filter(expense => expense.id !== id)
+    setExpenses(newExpense)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(charge !== "" && amount > 0){
+      const newExpense = {id: crypto.randomUUID(), charge, amount}
+
+      const newExpenses = [...expenses, newExpense];
+
+      setExpenses(newExpenses);
+      setCharge("");
+      
+    } else {
+      console.error('error');
+    }
+  }
+
+  
     return (
       <main className="main-container">
-        <div className="sub-continaer">
+        <div className="sub-container">
           <h1>장바구니</h1>
 
           <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem'}}>
-            {    } Expense Form
+            {    } <ExpenseForm charge={charge} handleSubmit={handleSubmit} handleCharge={handleCharge} amount={amount} handleAmount={handleAmount} />
           </div>
 
           <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem'}}>
-            {    } Expense List
+            {    }
+            <ExpenseList initialExpenses={expenses} handleDelete={handleDelete} />
           </div>
 
           <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
@@ -23,5 +64,6 @@ export default class App extends Component {
         </div>
       </main>
     )
-  }
 }
+
+export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './ExpenseList.css'
+import ExpenseItem from './ExpenseItem'
+
+
+const ExpenseList = ({initialExpenses, handleDelete}) => {
+  return (
+    <>
+    <ul className='List'>
+      {initialExpenses.map(expense => {
+        return (
+          <ExpenseItem key={expense.id} expense={expense} 
+          handleDelete={handleDelete} />
+        )
+      })}
+    </ul>
+    <button className=''>
+      목록지우기
+    </button>
+  </>
+  )
+}
+export default ExpenseList
+
+ 
